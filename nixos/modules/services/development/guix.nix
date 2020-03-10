f3807aad389608a16803d413c98916718af5654f
Félix Baylac-Jacqué <felix@alternativebit.fr>
2020-03-10 23:26:07 +0100

    nixos/guix: using systemd features to replace activation script
    We're leveraging systemd's tmpfiles to define the directories we want
    to create and the paths we want to simlimk.
    
    We then use a pre-start service hook to copy the bootstrap guix store
    and set the default binary cache substituer. This script is only
    supposed to be run after a guix version change. We store the current
    guix store path in a file to keep track of such a change.
    
    For /var/guix, we only symlink the paths we are interested in. This
    prevents us to erase any stateful thing (gcroot, profile or sqlite DB)
    during a service restart.

--- nixos/modules/services/development/guix.nix
+++ nixos/modules/services/development/guix.nix
@@ -5,6 +5,7 @@ with lib;
 let
 
   cfg = config.services.guix;
+  rsync = "${pkgs.rsync}/bin/rsync";
 
   buildGuixUser = i:
     {
@@ -17,6 +18,37 @@ let
         isSystemUser = true;
       };
     };
+
+  bootstrap-guix-paths = pkgs.writeScriptBin "bootstrap-guix-paths" ''
+      #!/usr/bin/env bash
+
+      # We do not want to run this script on earch service start, we
+      # only want to run it if the guix nix package has been updated,
+      # meaning a new guix bootstrap store needs to merged to the
+      # existing one.
+      #
+      # We're using the /var/guix/guix_nix_store_path file to track
+      # the guix nix store path we had the last time we ran this
+      # script. If the path changed, we need to update the bootstrap
+      # store and the default substitutes. If it didn't, this script
+      # should be no-op.
+
+      latestGuixKnownPath="/var/guix/guix_nix_store_path"
+      if ! [[ -f "''${latestGuixKnownPath}" ]] || ! grep -q "${pkgs.guix}" "''${latestGuixKnownPath}"
+        then
+          # Merging both the store and the /var/guix coming from the
+          # package to the current system.
+          ${rsync} -a --ignore-existing ${cfg.package.store}/gnu/store /gnu/
+
+          # We set $GUIX_PROFILE to use the /root/../current instead of
+          # the one embedded in the guix store path of the guix binary.
+          GUIX_PROFILE="/root/.config/guix/current"; source $GUIX_PROFILE/etc/profile
+          # authorize substitutes
+          guix archive --authorize < /root/.config/guix/current/share/guix/ci.guix.info.pub
+          echo ${pkgs.guix} > "''${latestGuixKnownPath}"
+      fi
+  '';
+
 
 in {
 
@@ -37,10 +69,22 @@ in {
       extraGroups.guixbuild = {name = "guixbuild";};
     };
 
+    systemd.tmpfiles.rules = [
+      #t  path                                            user gr   Age Arg
+      "d  '/gnu/store'                                    root root -   -"
+      "d  '/root/.config/guix'                            root root -   -"
+      "d  '/var/guix/gcroots'                             root root -   -"
+      "L+ '/var/guix/gcroots/profiles                     root root -  '${guix.var}/var/guix/gcroots/profiles'"
+      "d  '/var/guix/gprofiles/per-user/root'             root root -   -"
+      "L+ '/var/guix/profiles/per-user/root/current-guix' root root -  '${guix.var}/var/guix/profiles/per-user/root/current-guix'"
+      "L+ '/root/.config/guix/current'                    root root -  '${guix.var}/var/guix/profiles/per-user/root/current-guix'"
+    ];
+
     systemd.services.guix-daemon = {
       enable = true;
       description = "Build daemon for GNU Guix";
       serviceConfig = {
+        ExecStartPre = "${bootstrap-guix-paths}/bin/boostrap-guix-paths";
         ExecStart="/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild";
         Environment="GUIX_LOCPATH=/var/guix/profiles/per-user/root/guix-profile/lib/locale";
         RemainAfterExit="yes";
@@ -53,39 +97,9 @@ in {
       wantedBy = [ "multi-user.target" ];
     };
 
-    system.activationScripts.guix = ''
-
-      # copy initial /gnu/store
-      if [ ! -d /gnu/store ]
-      then
-        mkdir -p /gnu
-        cp -ra ${cfg.package.store}/gnu/store /gnu/
-      fi
-
-      # copy initial /var/guix content
-      if [ ! -d /var/guix ]
-      then
-        mkdir -p /var
-        cp -ra ${cfg.package.var}/var/guix /var/
-      fi
-
-      # root profile
-      if [ ! -d ~root/.config/guix ]
-      then
-        mkdir -p ~root/.config/guix
-        ln -sf /var/guix/profiles/per-user/root/current-guix \
-          ~root/.config/guix/current
-      fi
-
-      # authorize substitutes
-      GUIX_PROFILE="`echo ~root`/.config/guix/current"; source $GUIX_PROFILE/etc/profile
-      guix archive --authorize < ~root/.config/guix/current/share/guix/ci.guix.info.pub
-    '';
-
     environment.shellInit = ''
       # Make the Guix command available to users
       export PATH="/var/guix/profiles/per-user/root/current-guix/bin:$PATH"
-
       export GUIX_LOCPATH="$HOME/.guix-profile/lib/locale"
       export PATH="$HOME/.guix-profile/bin:$PATH"
       export INFOPATH="$HOME/.guix-profile/share/info:$INFOPATH"
