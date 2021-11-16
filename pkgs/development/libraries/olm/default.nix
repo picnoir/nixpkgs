1ec27e662590dc4c9af6c431fa49ea59e6325aae
Félix Baylac-Jacqué <felix@alternativebit.fr>
2021-11-16 15:56:19 +0100

    olm wasm: wip

--- pkgs/development/libraries/olm/default.nix
+++ pkgs/development/libraries/olm/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitLab, cmake }:
+{ lib, stdenv, fetchFromGitLab, cmake, emscripten, python3 }:
 
 stdenv.mkDerivation rec {
   pname = "olm";
@@ -12,9 +12,24 @@ stdenv.mkDerivation rec {
     sha256 = "1srmw36nxi0z2y5d9adks09p950qm0fscbnrq1fl37fdypvjl1sk";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ emscripten python3 ];
 
-  doCheck = true;
+  postUnpack = ''
+    patchShebangs $PWD
+  '';
+
+  makeFlags = [ "PREFIX='./dist'" "js" ];
+
+  # Emscripten is trying to cache the system calls in the nix store by default.
+  # Overriding that and forcing the cache to happen from within the builder.
+  EM_CACHE = "./cache";
+
+  postInstall = ''
+    mkdir $out/lib
+    cp -r javascript/* $out/lib
+  '';
+
+  doCheck = false;
 
   meta = with lib; {
     description = "Implements double cryptographic ratchet and Megolm ratchet";
